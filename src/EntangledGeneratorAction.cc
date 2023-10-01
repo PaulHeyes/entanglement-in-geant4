77ba83bcc7c4a93d36aa56cb2ceb7042901cd2ef
PaulHeyes <143719605+PaulHeyes@users.noreply.github.com>
2023-10-01 17:17:10 +0200

    Update EntangledGeneratorAction.cc

--- src/EntangledGeneratorAction.cc
+++ src/EntangledGeneratorAction.cc
@@ -23,12 +23,19 @@
 // * acceptance of all terms of the Geant4 Software license.          *
 // ********************************************************************
 
-//Paul Heyes, 16.09.2015
-//Brief implementation of the EntangledGeneratorAction class
+// Paul Heyes, 2019
+// Brief implementation of the EntangledGeneratorAction class
 
 /*
-In the constructor, the values for the second gamma particle of the same energy, opposite momentum direction (toward the second scatterer) and perpendicular polarisation, compared to the first particle created in 'BellsPrimaryGeneratorAction' are set and the particle origin placed in the centre of the world. 
-Again, the 'physical' event is created in the GeneratePrimaries() function. In case the 'first' events have been created with random direction and polarisation, these values must be passed on (as arguments maybe? - then the function must be also changed where called in the QERunManager) and the opposite/perpendicular values set for the second photons.
+Instead of generating actual annihilations in the Geant4 framework, an easier route is taken here,
+namely, two photons with opposite momentum direction and respective energy of 511 keV are created.
+
+In the constructor, the values for the second gamma particle of the same energy, opposite momentum direction 
+(toward the second scatterer) and perpendicular polarisation, compared to the first particle created in 
+'BellsPrimaryGeneratorAction' are set and the particle origin placed in the centre of the world. 
+Again, the 'physical' event is created in the GeneratePrimaries() function. In case the 'first' events have 
+been created with random direction and polarisation, these values must be passed on (as arguments maybe?
+- then the function must be also changed where called in the QERunManager) and the opposite/perpendicular values set for the second photons.
 */
 
 #include "EntangledGeneratorAction.hh"
@@ -51,7 +58,7 @@ Again, the 'physical' event is created in the GeneratePrimaries() function. In c
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-//Constructor
+// Constructor
 EntangledGeneratorAction::EntangledGeneratorAction()
   : BellsPrimaryGeneratorAction(),
     fParticleGun(0)
@@ -80,7 +87,7 @@ EntangledGeneratorAction::EntangledGeneratorAction()
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-//Destructor
+// Destructor
 EntangledGeneratorAction::~EntangledGeneratorAction()
 {
     delete fParticleGun;
@@ -90,70 +97,8 @@ EntangledGeneratorAction::~EntangledGeneratorAction()
 
 
 void EntangledGeneratorAction::GeneratePrimaries(G4Event* anEvent)
-{
-  //G4cerr << "here" << G4endl;
-  /* 
-  // Randomized Polarization
-  //
-  G4double phi = G4UniformRand()*twopi *rad;
-  G4double theta = G4UniformRand()*pi *rad;
-  G4double x0  = std::sin(theta)*std::cos(phi);
-  G4double y0  = std::sin(theta)*std::sin(phi);
-  G4double z0  = std::cos(theta); 
-  // x0 += dx0*(G4UniformRand()-0.5);
-  //  y0 += dy0*(G4UniformRand()-0.5);
-  // z0 += dz0*(G4UniformRand()-0.5);
-  fParticleGun->SetParticlePolarization(G4ThreeVector(x0,y0,z0));
-  fParticleGun2->SetParticlePolarization(G4ThreeVector(-x0,-y0,-z0));
-  */
-/*
-  // Perpendicular polarization to 'first correlated photon'
-  // This could be done a lot simpler, given that we are essentially dealing with a two-component vector ^^
-  extern G4ThreeVector QEPolarization;
-
-  G4double x0 = QEPolarization.x();
-  G4double y0 = QEPolarization.y();
-
-  G4cout << "Pol(x) = " << QEPolarization.x() << ", Pol(y) = " << QEPolarization.y() << G4endl;
-
-  G4double r1 = G4UniformRand()*twopi;
-  G4double x1;
-  G4double y1;
-
-  if(x0 == 0){
-      y1 = 0;
-      if(std::sin(r1) < 0) x1 = -1;
-      else x1 = 1;
-  }
-  else if(y0 == 0){
-      x1 = 0;
-      if(std::sin(r1) < 0) y1 = -1;
-      else y1 = 1;
-  }
-
-  x1 = std::sin(r1);
-  y1 = (-1)*x0*x1/y0;
-
-  //G4cout << "x1: " << x1 << " y1: " << y1 << G4endl;
-
-  G4ThreeVector NewPolarization = G4ThreeVector(x1,y1,0).unit();
-  x1 = NewPolarization.x();
-  y1 = NewPolarization.y();
-
-  // Test
-  G4AnalysisManager* analysisManager = G4AnalysisManager::Instance();
-  analysisManager->FillH2(0, x0, y0);
-  analysisManager->FillH2(1, x1, y1);
-
-  //G4double angle = NewPolarization.angle(QEPolarization);
-  //G4double scalarProd = NewPolarization.dot(QEPolarization);
-
-  //G4cout << "NewPol(x) = " << x1 << ", NewPol(y) = " << y1 << G4endl;
-  //G4cout << "Angle = " << angle << ", Scalar Product = " << scalarProd << G4endl;
-
-  fParticleGun->SetParticlePolarization(NewPolarization);
-*/
-  //Create the vertex of the particle passing it to the G4Event pointer
+{ 
+  // Create the vertex of the particle passing it to the G4Event pointer
   fParticleGun->GeneratePrimaryVertex(anEvent);
 }
 
